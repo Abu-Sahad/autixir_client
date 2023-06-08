038a21e9680f30e3472d69bada6c181abcc9aa77
Abu-Sahad <abusahad603@gmail.com>
2023-06-08 17:53:21 +0600

    banner added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
-import Home from "../Pages/Home/Home";
+import Home from "../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
@@ -8,7 +8,7 @@ const router = createBrowserRouter([
         element: <Main></Main>,
         children: [
             {
-                path: './',
+                path: '/',
                 element: <Home></Home>,
             },
         ],
